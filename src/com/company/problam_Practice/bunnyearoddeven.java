a75260ba7194d78475747c64905584e192aa715e
vishalrtiwari <vishalratantiwari@gmail.com>
2019-02-24 12:00:36 +0530

    bunny ear count odd even

---
+++ src/com/company/problam_Practice/bunnyearoddeven.java
@@ -0,0 +1,26 @@
+package com.company.problam_Practice;
+
+ class Main{
+
+        public static int Bunnyear(int n) {
+            if (n < 1) {
+                return -1;
+            }
+            if(n == 1){
+                return 1;
+
+            }
+           if(n%2 == 0){
+                return Bunnyear(n-1) + 2;
+            }
+            else{
+                return Bunnyear(n-1) +1;
+           }
+        }
+        public static void main(String [] args){
+
+            System.out.println(  Bunnyear(6));
+        }
+    }
+
+
