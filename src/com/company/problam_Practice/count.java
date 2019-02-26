70f040ca4ba94b5d0b3dffaa55f0e9c0b67bb92e
vishalrtiwari <vishalratantiwari@gmail.com>
2019-02-26 23:22:46 +0530

    count total x recursively

--- src/com/company/problam_Practice/count.java
+++ src/com/company/problam_Practice/count.java
@@ -1,17 +1,33 @@
 package com.company.problam_Practice;
 
-public class count {
-    public static int count7(int n) {
-        if (n <= 0) {
+
+class count {
+    public int countX(String str) {
+
+
+        if (str == str.substring(0, 0)) {
             return 0;
-        }
-        if (n % 10 == 7) {
-            return count7(n / 10) + 1;
         } else {
-            return count7(n / 10) + 0;
+            int n = str.length();
+
+            if (str.charAt(n-1) == 'x') {
+
+                str = str.substring(0,n - 1);
+
+                return countX(str) + 1;
+            } else {
+                str = str.substring(0,n - 1);
+
+                return countX(str) ;
+            }
         }
     }
-    public static void main(String [] args){
-        System.out.println(count7(7117));
+    public static  void  main(String[] args){
+        count obj = new count();
+        System.out.println(obj.countX("iiixxxxxxxxxiii"));
     }
+
 }
+
+
+
